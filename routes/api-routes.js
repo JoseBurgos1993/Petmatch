aafd6c18c229384ddc49f7958d8cfe96987e02a9
JoseBurgos1993 <burgosjd1993@gmail.com>
2020-08-27 17:35:13 -0500

    Updated handlebars

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,7 @@
 // Requiring our models and passport as we've configured it
 const db = require("../models");
 const passport = require("../config/passport");
-
+const nodeMailer = require("nodeMailer");
 module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
@@ -43,6 +43,31 @@ module.exports = function(app) {
   app.get("/logout", (req, res) => {
     req.logout();
     res.redirect("/");
+  });
+
+  app.post('/api/send-email', function (req, res) {
+    let transporter = nodeMailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            // should be replaced with real sender's account
+            user: 'donotreply.to.PetMatch@gmail.com',
+            pass: '1+sCH33P\\/\/ine'
+        }
+    });
+    let mailOptions = {
+        // should be replaced with real recipient's account
+        to: req.body.email,//'patelcatalina@gmail.com',
+        subject: req.body.subject,
+        text: req.body.message
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+    });
   });
 
   // Route for getting some data about our user to be used client side
